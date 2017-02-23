8b957c6ccfb1d8faac810192a54fe6f29eff19ae
GearShift24 <gearshift24@gmail.com>
2017-02-23 08:36:33 -0700

    added lists

--- Mega/Model/List.hpp
+++ Mega/Model/List.hpp
@@ -19,6 +19,7 @@ class Array
 {
 private:
     Node<Type> * front;
+    Node<Type> * end;
     int size;
     
 public:
@@ -28,22 +29,75 @@ public:
     ~List<Type>();
     
     //methods
+    
+    int getSize() const;
+    Node<Type> * getFront() const;
+    Node<Type> * getEnd() const;
+    
+    
     void addAtIndex(int index, Type value);
     void add(Type value);
     Type remove(int index);
     Type setAtIndex(int index, Type data);
     Type getFromIndex(int index);
     bool contains(Type data);
-    int getSize() const;
-    Node<Type> * getFront() const;
-};
+    
+    
+
+
+   };
+
+
+//constructors implemented
+template <class Type>
+List<Type> :: List()
+{
+    this->size = 0;
+    this->front = nullptr;
+    this->end = nullptr;
+}
+//
+//template <class Type>
+//List<Type> :: List(const List<Type> & source)
+//{
+//    
+//}
+//
+//template <class Type>
+//List<Type> :: ~List()
+//{
+//    
+//}
 
 
 
 
 
-
-
+//methods implemented
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size ==0)
+       {
+           //gives you the way to get to the front here, then you add l8r g8r
+           Node<Type> * first = new Node(value);
+           this->front = first;
+           this->end = first;
+       }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->setNodePointer(front);
+        //or
+//        Node<Type> * newFirst = new Node<Type>(value, front);
+        front = newFirst;
+        
+        
+    }
+    
+    size++;
+}
 
 
 
