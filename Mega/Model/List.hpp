76ed1ec502be2313ad3959cde14b3523e1bb3c81
GearShift24 <gearshift24@gmail.com>
2017-02-23 09:53:25 -0700

    added remove

--- Mega/Model/List.hpp
+++ Mega/Model/List.hpp
@@ -92,8 +92,6 @@ void List<Type> :: addFront(Type value)
         //or
 //        Node<Type> * newFirst = new Node<Type>(value, front);
         front = newFirst;
-        
-        
     }
     
     size++;
@@ -101,4 +99,144 @@ void List<Type> :: addFront(Type value)
 
 
 
+
+template <class Type>
+void List<Type> :: addEnd(Type data)
+{
+    
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this-front = added;
+        this->end = added;
+    }
+    
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index <= size);
+    if(index == 0)
+    {
+        addFront(value);
+    
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+
+    }
+    else
+    {
+        Node<Type> * insertNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        
+        for(int position = -; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        previous->setNodePointer(insertedNode);
+        insertedNode->setNodePointer(current);
+        
+        size++;
+    }
+        
+                  
+    
+}
+
+template <class Type>
+Type List<Type> :: getFromIndex(int index)
+{
+    assert(index >=0 && index < size);
+    Type value;
+    
+    Node<Type> * current = front;
+    
+    for(int position = 0; position < index; position++)
+    {
+        current = current->getNodePointer();
+    }
+    value = current->getNodeData();
+    return value;
+}
+
+
+template <class Type>
+Type List<Type> :: setAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index < size);
+    Node<Type> * current = front;
+    for(int position = 0; position < index; position++)
+    {
+        current = current->getNodePointer();
+    }
+    
+    current->setNodeData(value);
+}
+
+
+template <class Type>
+Type List<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < size);
+    Type removed;
+    
+    Node<Type> * current = front;
+    Node<Type> * previous = nullptr;
+    Node<Type> * toBeRemoved = nullptr;
+    
+    
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        front = front->getNodePointer();
+    }
+    else if(index == size - 1)
+    {
+        for(int spot 0; spot < index; spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        current->setNodePointer(nullptr);
+        this->end = previous;
+        
+        
+    }
+    
+    else{
+        for(int spot 0; spot < index; spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        current = toBeRemoved->getNodePointer();
+        previous->setNodePointer(current);
+    }
+    
+        removed = toBeRemoved->getNodeData();
+    
+    delete toBeRemoved;
+    
+    
+    size--;
+    return removed;
+}
 #endif /* List_hpp */
