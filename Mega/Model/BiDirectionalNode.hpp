33d35ca526214edfcf920fbdbbfdc929ee9b65c2
GearShift24 <gearshift24@gmail.com>
2017-02-27 09:54:48 -0700

    bi node made

--- Mega/Model/BiDirectionalNode.hpp
+++ Mega/Model/BiDirectionalNode.hpp
@@ -15,13 +15,36 @@ class BiDirectionalNode : public Node<Type>
 {
 private:
     Type data;
-    
+    BiDirectionalNode<Type> * next;
+    BiDirectionalNode<Type> * previous;
 public:
+    BiDirectionalNode();
+    BiDirectionalNode(Type data);
+    BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next);
     
     
+    Type getNodeData();
+    BiDirectionalNode<Type> * getNextPointer();
+    BiDirectionalNode<Type> * getPreviousPointer();
     
+    void setNodeData(Type data);
+    void setNextPointer(BiDirectionalNode<Type> * next);
+    void setPreviousPointer(BiDirectionalNode<Type> * previous);
 };
 
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node()
+{
+    
+}
+
+
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode(Type data) : Node(data)
+{
+    
+}
 
 
 #endif /* BiDirectionalNode_h */
